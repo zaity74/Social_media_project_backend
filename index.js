d8539ce4e8e0f703b413009007a5fb0caf993195
afouq <afouquet67@gmail.com>
2025-03-10 12:30:21 +0100

    accès variable environnement good

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ import dotenv from "dotenv";
 
 const app = express();
 
+dotenv.config();
 const port = process.env.PORT;
 console.log(port);
 app.use(express.json());
 
 // app.use(routes);
 
-app.listen(8081, () =>
-    console.log(`Le serveur est a l'écoute sur le port ${8081}`)
+app.listen(port, () =>
+    console.log(`Le serveur est a l'écoute sur le port ${port}`)
 );
